e71f2ca652352dae37b358adf779b36899016290
Piotr Zajac <ptr.zjc@gmail.com>
2023-02-28 23:04:25 +0100

    Update TM winrates

--- terraforming-mars/2-prepare-to-visualise.sh
+++ terraforming-mars/2-prepare-to-visualise.sh
@@ -17,7 +17,7 @@ prepare_as_csv() {
         mlr --j2c --ho cat
 }
 
-prepare_as_csv 0 | termgraph > winrate_2pl.txt
-prepare_as_csv 1 | termgraph > winrate_3pl.txt
-prepare_as_csv 2 | termgraph > winrate_4pl.txt
-prepare_as_csv 3 | termgraph > winrate_5pl.txt
+prepare_as_csv 0 | termgraph --label-before > winrate_2pl.txt
+prepare_as_csv 1 | termgraph --label-before > winrate_3pl.txt
+prepare_as_csv 2 | termgraph --label-before > winrate_4pl.txt
+prepare_as_csv 3 | termgraph --label-before > winrate_5pl.txt
